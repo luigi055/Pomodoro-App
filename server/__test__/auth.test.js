2b92ed8c68355bf74ed86f132c6d3729df8bfee0
Luigi Doganieri <luigi5021@gmail.com>
2017-10-15 23:28:22 -0400

    Pomodoro API Ready

--- server/__test__/auth.test.js
+++ server/__test__/auth.test.js
@@ -8,19 +8,3 @@ const expect = chai.expect;
 chai.use (chaiHTTP);
 
 beforeEach (populateUsers);
-
-describe ('PATCH /auth/user/update', () => {
-  it ('should change user name', done => {
-    chai
-      .request (app)
-      .patch ('/auth/user/update')
-      .send ({
-        name: 'Pedro',
-      })
-      .end ((end, res) => {
-        res.user = users[0];
-        expect (res).to.have.status (200);
-        done ();
-      });
-  });
-});
